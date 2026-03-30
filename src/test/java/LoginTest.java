43c17ace1953c4e5a80f50af3a2e761d802186e1
Alex <user.trail@gmail.com>
2026-03-31 00:00:29 +0300

    Второй тест на логин

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,24 +1,20 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
+
 import static org.testng.Assert.assertEquals;
 
 public class LoginTest {
 
     @Test
     public void checkLogin() {
-        ChromeOptions options = new ChromeOptions();
-
-        WebDriver browser = new ChromeDriver(options);
+        WebDriver browser = new ChromeDriver();
 
         try {
             browser.get("https://www.saucedemo.com/");
 
-            browser.findElement(By.id("user-name")).clear();
             browser.findElement(By.id("user-name")).sendKeys("standard_user");
-
             browser.findElement(By.id("password")).sendKeys("secret_sauce");
             browser.findElement(By.id("login-button")).click();
 
